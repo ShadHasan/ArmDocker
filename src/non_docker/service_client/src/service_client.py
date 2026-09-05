a8d63cda8ba539329bd1507c1f78c0a1028a2aff
Shad Hasan <baksh12shad@gmail.com>
2026-09-06 00:37:11 +0530

    small way to go build service client

---
+++ src/non_docker/service_client/src/service_client.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "This script is valid for debian based linux distros."
+
+
+SERVICE_CLIENT_PATH=""
+
+SERVICE_CLIENT_SERVICE_PATH=""  # This generally reside within init.d
+
+# Creating virtual environment
+
+# SCP the service-client, run script script at ${SERVICE_CLIENT_PATH}
+
+# service-client.py: actually running service-client
+# run.sh take care of loading, activating python virtual environment in which service-client.py will be running.
